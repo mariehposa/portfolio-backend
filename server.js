648fdea032c4d8c44c36cd61411e2a824c212169
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-17 21:39:01 +0000

    create server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet')
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.json('Its working!')
+})
+
+module.exports = server;
