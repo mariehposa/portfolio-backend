e5ce9357a62f331e50521f97fdc8ca9a428f5a49
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-17 22:55:20 +0000

    contact endpoint created

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet')
 const server = express()
+const contactRouter = require('./contacts/contactRouter')
 
 server.use(helmet())
 server.use(express.json())
@@ -10,5 +11,7 @@ server.use(cors())
 server.get('/', (req, res) => {
     res.json('Its working!')
 })
+
+server.use('/api', contactRouter);
 
 module.exports = server;
