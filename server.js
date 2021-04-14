abaee988732ec7043300be1577fb2a1eb80be630
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-04-14 22:31:01 +0100

    update sqlite

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet')
-const server = express()
-const contactRouter = require('./contacts/contactRouter')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express();
+const contactRouter = require("./contacts/contactRouter");
 
-server.use(helmet())
-server.use(express.json())
-server.use(cors())
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
-server.get('/', (req, res) => {
-    res.json('Its working!')
-})
+server.get("/", (req, res) => {
+  res.json("Its working!");
+});
 
-server.use('/api', contactRouter);
+server.use("/api", contactRouter);
 
 module.exports = server;
